ba8ce99265f51a856168f16c3a5e2b63442e8d41
Camila Debage <camiladebage@hotmail.com>
2022-01-31 20:12:36 -0300

    commit aula de caracter e condicionais

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e textos");
+
+            char primeiraLetra = 'a';
+
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Camila Debage de Souza " + 2022;
+            string cursos = @" - teste 1  
+                - teste 2 
+                - teste 3";
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursos);
+            
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 7");
+
+            int idadeCamila = 16;
+            int qtdPessoas = 2;
+
+            if (idadeCamila >= 18)
+            {
+                Console.WriteLine("Camila possui + de 18 anos");
+            }
+            else
+            {
+                if(qtdPessoas >= 2)
+                {
+                    Console.WriteLine("Camila está acompanhada");
+                }
+                else
+                {
+                    Console.WriteLine("Camila não possui + de 18 anos");
+                }
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
